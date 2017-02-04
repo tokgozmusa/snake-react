c7aa6d0407e14fbee831cfaa47be77b2765c7410
tokgozmusa <tokgozmusa@gmail.com>
2017-02-05 01:16:30 +0300

    First commit

---
+++ src/components/NotFound/index.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './style.css';
+
+class NotFound extends Component {
+  render() {
+    return (
+      <div>
+          <h1>Sorry, page not found</h1>
+      </div>
+    );
+  }
+}
+
+export default NotFound;
