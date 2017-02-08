0016ab337e2b73b16a70cc44549b6cce9d4592f8
tokgozmusa <tokgozmusa@gmail.com>
2017-02-08 19:08:27 +0300

    Add .travis.yml

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Snake Game using React
 
+[![Build Status](https://travis-ci.org/tokgozmusa/snake-react.svg?branch=master)](https://travis-ci.org/tokgozmusa/snake-react)
+
 Snake game will be alive soon :)
 
 http://snake_react.surge.sh/
--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -11,7 +11,7 @@ class App extends Component {
                     <h2>Welcome to React</h2>
                 </div>
                 <p className="App-intro">
-                    Snake will move here soon :)
+                    Snake will move here soon :) version-1.0
                 </p>
             </div>
         );
