7708b4dff2c98656c38ae82edd57558e0d46feb9
tokgozmusa <tokgozmusa@gmail.com>
2017-02-08 19:05:58 +0300

    Edit

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
+import logo from './logo.svg';
 import './style.css';
 
 class App extends Component {
     render() {
         return (
-            <div>
-                {this.props.children}
+            <div className="App">
+                <div className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h2>Welcome to React</h2>
+                </div>
+                <p className="App-intro">
+                    Snake will move here soon :)
+                </p>
             </div>
-
         );
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Routes from './routes';
+import App from './components/App'
 import './index.css';
 
 ReactDOM.render(
-    <Routes/>, document.getElementById('root')
+    <App/>, document.getElementById('root')
 );
