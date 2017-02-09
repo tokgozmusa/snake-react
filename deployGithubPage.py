60bebee213bf3446c391b78d6a18b5071ba71423
tokgozmusa <tokgozmusa@gmail.com>
2017-02-09 07:25:51 +0300

    Fix

--- deployGithubPage.py
+++ deployGithubPage.py
@@ -2,22 +2,22 @@ import json
 import os
 
 def main():
-    with open("package.json", encoding = "utf-8") as data_file:
+    with open("package.json", "r", encoding = "utf-8") as data_file:
         data = json.loads(data_file.read())
 
     data["homepage"] = "http://tokgozmusa.github.io/snake-react"
 
-    with open("package.json", "w") as outfile:
-        json.dump(data, outfile, indent = 2)
-        print("", file = outfile)
+    with open("package.json", "w") as output_file:
+        json.dump(data, output_file, indent = 2)
+        print("", file = output_file)
 
     os.system("npm run deploy")
 
     del data["homepage"]
 
-    with open("package.json", "w") as outfile:
-        json.dump(data, outfile, indent = 2)
-        print("", file = outfile)
+    with open("package.json", "w", encoding = "utf-8") as output_file:
+        json.dump(data, output_file, indent = 2)
+        print("", file = output_file)
 
 if __name__ == "__main__":
     main()
