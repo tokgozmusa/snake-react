3f58151f702be631f14eba1fd655d0a52e8666cb
tokgozmusa <tokgozmusa@gmail.com>
2017-02-09 07:46:44 +0300

    Update v1

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -14,7 +14,7 @@ class App extends Component {
                     Snake will move here soon :)
                 </p>
                 <div>
-                    Hello world!
+                    Hello world! - v1
                 </div>
             </div>
         );
